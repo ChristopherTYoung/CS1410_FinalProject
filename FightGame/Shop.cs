ee4e8d4bd028c3153a74155a1f9438c28c89ef1e
Ricardo Ruiz <ricardo_ruizjr@hotmail.com>
2022-11-21 13:53:16 -0700

    Just added a Shop class with a few properties

---
+++ FightGame/Shop.cs
@@ -0,0 +1,7 @@
+public class Shop
+{
+    public static int Money { get; set; }
+    public static int UpgradeDamage { get; set; }
+    // public static List<> Items { get; set;}
+    // Items, Set amount of upgrade damage?
+}
