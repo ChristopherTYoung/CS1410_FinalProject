354cbe0c02645175e4b41c0cd3556bedba9f5e77
Ricardo Ruiz <ricardo_ruizjr@hotmail.com>
2022-11-25 14:54:31 -0700

    Just wrote a comment in the shop class

--- FightGame/Shop.cs
+++ FightGame/Shop.cs
@@ -4,4 +4,5 @@ public class Shop
     public static int UpgradeDamage { get; set; }
     // public static List<> Items { get; set;}
     // Items, Set amount of upgrade damage?
+    // Items: potions, different weapons such as staffs, swords, spears, etc. , armor (if we wanna implement that), stat upgrades?
 }
