40457cee639132cfc1bede243a371512fca626bd
Ricardo Ruiz <ricardo_ruizjr@hotmail.com>
2022-12-10 14:05:20 -0700

    Got rid of Shop class cause not needed

--- FightGame/Shop.cs
+++
@@ -1,7 +0,0 @@
-public class shop
-{
-    public static string DodgePotion { get; set; }
-    public static string AttackPotion { get; set; }
-    public static string HealthBoost { get; set; }
-    public static string AttackBoost { get; set; }
-}
